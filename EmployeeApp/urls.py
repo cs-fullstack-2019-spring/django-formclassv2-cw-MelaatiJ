7616d4532a8a74c9ddbc643984097877b2d9b64d
MelaatiJ <fathimajayah876@gmail.com>
2019-02-27 13:46:48 -0600

    seperated my functions

--- EmployeeApp/urls.py
+++ EmployeeApp/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("apply/", views.apply, name="apply"),
-    # path("applicant/", views.applicant, name="applicant")
+    path("applicant/", views.applicant, name="applicant")
     # path("applicant/", views.applicant, name="applicant"),
 
 ]
